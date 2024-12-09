55cf05fa9f077153cfba57f5a8fa206d8ec15dc2
David Jelenc <david.jelenc@fri.uni-lj.si>
2024-12-10 00:52:28 +0100

    Pripravi zacetno kodo

--- app/src/main/java/ep/ws/MainActivity.kt
+++ app/src/main/java/ep/ws/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -20,20 +17,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil3.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SearchUI(HitsViewModel())
+            SearchUI()
         }
     }
 }
 
 @Composable
-fun SearchUI(viewModel: HitsViewModel) {
+fun SearchUI() {
     val text = remember { mutableStateOf("") }
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -44,46 +39,17 @@ fun SearchUI(viewModel: HitsViewModel) {
             modifier = Modifier.fillMaxWidth()
         )
         Button(
-            { viewModel.search(text.value) },
+            { println("Pričnemo z iskanjem") },
             modifier = Modifier
                 .padding(5.dp)
                 .fillMaxWidth(),
             shape = RoundedCornerShape(10)
-        ) { Text("Išči (${viewModel.hits.value.size})") }
-        LazyColumn {
-            items(viewModel.hits.value) {
-                Hit(it)
-            }
-        }
-    }
-}
-
-@Composable
-fun Hit(hit: Hit) {
-    Row {
-        Column(modifier = Modifier.weight(1f)) {
-            AsyncImage(
-                model = hit.Poster,
-                contentDescription = null,
-                modifier = Modifier.padding(5.dp)
-            )
-        }
-        Column(modifier = Modifier.weight(4f)) {
-            Text(
-                hit.Title,
-                fontSize = 20.sp
-            )
-            Text(
-                "${hit.Type} (${hit.Year})",
-                fontSize = 15.sp
-            )
-        }
+        ) { Text("Išči") }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SearchUIPreview() {
-//    Hit(Hit())
-    SearchUI(HitsViewModel())
+    SearchUI()
 }
